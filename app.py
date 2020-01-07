93ca5cf9fea1f4699be25de2b546abc0b91fd9fd
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2020-01-07 14:56:31 -0500

    added student register link, copied old code

--- app.py
+++ app.py
@@ -12,6 +12,46 @@ DB_FILE = "HumanJello.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False) #open if file exists, otherwise create
 c = db.cursor() #facilitate db operations
 
+@app.route("/")
+def root():
+    return render_template("root.html")
+
+@app.route("/studentAccount")
+def studacc():
+    return render_template("register.html", student=True);
+
+@app.route("/registerStudent", methods=["POST"])
+def registerstudent():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        password2 = request.form['password2']
+        c.execute("SELECT username FROM users WHERE username = ?", (username, ))
+        a = c.fetchone()
+        if a != None: #checks for duplicate usernames
+            flash("Account with that username already exists")
+            return redirect(url_for('registerStudent'))
+        elif " " in username: #checks for spaces cause spaces suck
+            flash("Username cannot contain spaces")
+            return redirect(url_for('registerStudent'))
+        elif password != password2: #checks if both passwords are the same
+            flash("Passwords do not match")
+            return redirect(url_for('registerStudent'))
+        elif len(password) < 8: #passwords must have a minimum length of 8
+            flash("Password must be at least 8 characters in length")
+            return redirect(url_for('registerStudent'))
+
+        else: #successfully created an account
+            c.execute("INSERT INTO users VALUES (NULL, ?, ?)", (username, password))
+            dbfunctions.newUserTable(c, username)
+            db.commit()
+            flash("Successfuly created user")
+            return redirect(url_for('studentLogin'))
+    else:
+        flash("GET request")
+        return redirect(url_for('registerStudent'))
+
+
 if __name__ == "__main__":
     app.debug = True;
     app.run()
