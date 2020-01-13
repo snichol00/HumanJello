5c28e7fb8a790efe98bac3fa87f65631b090feb1
Lauren <lpehlivanian00@stuy.edu>
2020-01-12 22:26:17 -0500

    moving student and admin db functions into specific files

---
+++ adm_db.py
@@ -0,0 +1,15 @@
+# Standard Lib
+import sqlite3
+from sqlite3 import connect
+from re import search
+from numbers import Number
+# Flask Lib
+from flask import current_app, g
+
+DB_FILE = "HumanJello.db"
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()
+
+# insert an opportunity into the database
+def insertOp(c, org, pos, int, des, gra, loc, due, post, dat):
+    c.execute("INSERT into opportunities (organization, position, interests, description, grades, location, duedate, posted, dates) VALUES(?, ?);", (org, pos, int, des, gra, loc, due, pos, dat))
