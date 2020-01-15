2a3f6fb4dace13e81f08944977c7ee013b81b0cb
Lauren <lpehlivanian00@stuy.edu>
2020-01-14 22:28:49 -0500

    fixed update opportunity function, works

--- dbfunctions.py
+++ dbfunctions.py
@@ -58,6 +58,26 @@ def setup():
                 notes TEXT
                 );""")
 
+#GENERAL FUNCTIONS--------------------------
+def update_user(c, username, field, newvalue):
+    c.execute("UPDATE users SET %s = '%s' WHERE username = '%s'" % (
+                field,
+                newvalue,
+                username
+            )
+        )
+    return "Success"
+
+# gets a column of a given database given a conditional
+def get(tbl_name, column, conditional=""):
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()
+    c.execute("SELECT %s FROM %s %s" % (column, tbl_name, conditional))
+    values = c.fetchall()
+    c.close()
+    return [list(value) for value in values]
+
+#OPPORTUNITIES FUNCTIONS---------------------------
 # initialize opportunity based on required inputs
 def createOp(c, name, des, nine, ten, elev, twel):
     c.execute("INSERT INTO opportunities (name, description, gr9, gr10, gr11, gr12) VALUES (?, ?, ?, ?, ?, ?);", (name, des, nine, ten, elev, twel))
@@ -73,36 +93,27 @@ def getInterests(c, id):
     print(arr)
     return arr
 
+#updates a field of opportunity based on id
+def updateOp(c, id, field, new_val):
+    c.execute("UPDATE opportunities SET %s = '%s' WHERE opid = %d;" % (field, new_val, id))
+
 def addInterest(c, id, interest):
     c.execute("UPDATE opportunities SET %s = True WHERE opid = %d;" % (interest, id))
 
 def insertOp(c, name, int, des, link, cost, gra, loc, due, start, end, notes):
     c.execute("INSERT into opportunities (name, interests, description, link, cost, grades, location, duedate, posted, start_date, end_date, notes) VALUES(?, ?);", (name, int, des, link, cost, gra, loc, due, datetime.now(), start, end, notes))
 
+def getAllOps(c):
+    c.execute("SELECT * FROM opportunities;")
+    all = c.fetchall()
+    return all
+
+#STUDENT FUNCTIONS---------------------------
 def addStudent(c, user, hashp, disp, osisNum, emailAcc, gra, inter):
     c.execute("INSERT into users (username, hashpassword, displayname, osis, email, grade, interests, admin) VALUES(?, ?, ?, ?, ?, ?, ?, ?);", (user, hashp, disp, osisNum, emailAcc, gra, inter, False))
 
 def createStudent(c, user, hashp):
     c.execute("INSERT into users (username, hashpassword, admin) VALUES(?, ?, ?)", (user, hashp, False))
-
-
-def addAdmin(c, user, hashp, emailAcc):
-    c.execute("INSERT INTO users (username, hashpassword, email, admin) VALUES(?, ?, ?, ?);", (user, hashp, emailAcc, True))
-
-def isAdmin(c, username):
-    c.execute("SELECT admin FROM users WHERE username = ?", (username, ))
-    userinfo = c.fetchone()
-    print(userinfo)
-    return userinfo[0]
-
-def update_user(c, username, field, newvalue):
-    c.execute("UPDATE users SET %s = '%s' WHERE username = '%s'" % (
-                field,
-                newvalue,
-                username
-            )
-        )
-    return "Success"
 
 #return whether or not the student has filled in basic info yet
 def studentInit(c, username):
@@ -112,16 +123,12 @@ def studentInit(c, username):
         return True
     return False
 
-# gets a column of a given database given a conditional
-def get(tbl_name, column, conditional=""):
-    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-    c = db.cursor()
-    c.execute("SELECT %s FROM %s %s" % (column, tbl_name, conditional))
-    values = c.fetchall()
-    c.close()
-    return [list(value) for value in values]
+#ADMIN FUNCTIONS-----------------------------
+def addAdmin(c, user, hashp, emailAcc):
+    c.execute("INSERT INTO users (username, hashpassword, email, admin) VALUES(?, ?, ?, ?);", (user, hashp, emailAcc, True))
 
-def getAllOps(c):
-    c.execute("SELECT * FROM opportunities;")
-    all = c.fetchall()
-    return all
+def isAdmin(c, username):
+    c.execute("SELECT admin FROM users WHERE username = ?", (username, ))
+    userinfo = c.fetchone()
+    print(userinfo)
+    return userinfo[0]
