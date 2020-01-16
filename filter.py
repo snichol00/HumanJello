a04843232dc9be2d7263eb33e189a1411f58ccf1
Lauren <lpehlivanian00@stuy.edu>
2020-01-16 00:19:07 -0500

    moved filter out of utl folder, fixed errors in getrelops

--- filter.py
+++ filter.py
@@ -5,7 +5,7 @@ from re import search
 from numbers import Number
 # Flask Lib
 from flask import current_app, g
-import dbfunctions
+from dbfunctions import get, getGrade
 
 # should return all opportunities relevant to the user
 def relOps(username):
@@ -18,6 +18,8 @@ def relOps(username):
 
 # should return whether the opportunity is of interest to the given user
 def isOpRel(opID, username):
+    grade = getGrade(username)
+
     opEv = get("opportunities", "events", "WHERE opid = '%s'" % opID)[0][0]
     opAc = get("opportunities", "academic", "WHERE opid = '%s'" % opID)[0][0]
     opBu = get("opportunities", "business", "WHERE opid = '%s'" % opID)[0][0]
@@ -40,20 +42,16 @@ def isOpRel(opID, username):
     sHu = get("users", "humanities", "WHERE username = '%s'" % username)[0][0]
     sSc = get("users", "scholarships", "WHERE username = '%s'" % username)[0][0]
 
-    if (opEv != sEv and opAc != sAc and opBu != sBu and opCo != sCo and opLe != sLe and opMu != sMu
-        and opNa != sNa and opSt != sSt and opHu != sHu and opSc != sSc):
-        return False
-
     opNine = get("opportunities", "gr9", "WHERE opid = '%s'" % opID)[0][0]
     opTen = get("opportunities", "gr10", "WHERE opid = '%s'" % opID)[0][0]
     opEleven = get("opportunities", "gr11", "WHERE opid = '%s'" % opID)[0][0]
     opTwelve = get("opportunities", "gr12", "WHERE opid = '%s'" % opID)[0][0]
     sGrade = get("users", "grade", "WHERE username = '%s'" % username)[0][0]
 
-    if (grade == "9" and !opNine):
-        if (grade == "10" and !opTen):
-            if (grade == "11" and !opEleven):
-                if (grade == "12" and !opTwelve):
-                    return False;
+    if (grade == "9" and not opNine) or (grade == "10" and not opTen) or (grade == "11" and not opEleven) or (grade == "12" and not opTwelve):
+        return False
 
-    return True
+    if (opEv and sEv) or (opAc and sAc) or (opBu and sBu) or (opCo and sCo) or (opLe and sLe) or (opMu and sMu) or (opNa and sNa) or (opSt and sSt) or (opHu and sHu) or (opSc and sSc):
+        return True
+
+    return False
