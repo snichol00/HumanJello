55bdec2aefb7709065bef22c7bf16af54ce306e1
Jason Zheng <jzheng02@stuy.edu>
2020-01-24 02:55:29 -0500

    Tried to do last min js

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from google.auth.transport.requests import Request
 from flask import Flask, render_template, redirect, url_for, session, flash, request
 import json, sys
 import sqlite3, os
-from datetime import datetime
 
 import urllib.request as urlrequest
 from urllib.request import urlopen, Request
@@ -411,7 +410,37 @@ def showCalendar():
 
 @app.route("/addEvent")
 def event():
-    return render_template("addEvent.html")
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    return render_template("addEvent.html", stuEvents = events)
 
 @app.route("/adminEvent")
 def addEvent():
